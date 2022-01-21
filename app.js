1ac633a502325911e4e63c12052e0c00376b8638
SharjeelPC <sharjeelpc1999@gmail.com>
2022-01-21 08:53:42 +0500

    11-1-22

---
+++ app.js
@@ -0,0 +1,60 @@
+// Rest Operator
+// function sum(v1,...args){
+//     var sum =0;
+//     for(var i in args){
+//         sum +=args[i];
+//     }
+//     console.log(v1+sum)
+
+// }
+// sum("sharjeel baba",20,30);
+// sum("sharjeel baba",20,30);
+// sum("sharjeel baba",20,30);
+// sum("sharjeel baba",20,30);
+// sum("sharjeel baba",20,30);
+
+
+// function sum(v1,...args){
+//     var sum =0;
+//     for(var i in args){
+//         sum +=args[i];
+//     }
+//     console.log(`${v1}`+" "+sum)
+
+// }
+// sum("sharjeel",10,5);
+// sum("sharjeel",60,40);
+// sum("sharjeel",20,30);
+
+//Spread Operator 
+
+
+// function sum(v1,...args){
+//     var sum =0;
+//     for(var i in args){
+//         sum +=args[i];
+//     }
+//     console.log(`${v1}`+" "+sum)
+
+// }
+// var arr=[2,3,4,5,6,7];
+// sum("sharjeel",...arr)
+
+
+
+// var arr=[1,2,3,4,5,6,7,8,9,0];
+// console.log(...arr)
+
+
+
+
+// function sum(v1,...args){
+//     var sum =0;
+//     for(var i in args){
+//         sum +=args[i];
+//     }
+//     console.log(`${v1}`+" "+sum)
+
+// }
+// var arr=[2,3,4,5,6,7];
+// sum("sharjeel",...arr)
